f85a2e9d0113888d14eddc2bae85e6afc88f9801
Michael Hu <pxmyhdev@gmail.com>
2019-12-13 19:00:38 +0800

    get parsing xls file working

--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,51 @@
 var express = require('express');
-// var formidable = require('formidable');
 var router = express.Router();
+var formidable = require('formidable');
+var util = require('util');
 
-/* GET home page. */
-router.get('/upload', function(req, res, next) {
-  res.send('processing file ...');
+/* GET file listing. */
+router.get('/', function(req, res, next) {
+  res.writeHead(200, { 'content-type': 'text/html' });
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">' +
+      '<input type="text" name="title"><br>' +
+      '<input type="file" name="upload" multiple="multiple"><br>' +
+      '<input type="submit" value="Upload">' +
+      '</form>'
+  );
+});
+
+router.get('/files', function(req, res, next) {
+  if (typeof require !== 'undefined') {
+    XLSX = require('xlsx');
+    console.log('initiating xlsx');
+  }
+  var workbook = XLSX.readFile('files/2019.9.30工程项目明细表.xls');
+  console.log(`read workbook finished. workbook: ${workbook}`);
+  var result = {};
+  workbook.SheetNames.forEach(function(sheetName) {
+    console.log(`sheetname = ${sheetName}`);
+    var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {
+      header: 1
+    });
+    console.log(`roa: ${roa}`);
+    if (roa.length) result[sheetName] = roa;
+  });
+  res.send(JSON.stringify(result, 2, 2));
+});
+
+router.post('/', (req, res, next) => {
+  // parse a file uploaded
+  var form = new formidable.IncomingForm();
+
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {
+      'content-type': 'text/plain'
+    });
+    res.write('received upload:\n\n');
+    // res.end();
+    res.end(util.inspect({ fields: fields, files: files }));
+  });
 });
 
 module.exports = router;
