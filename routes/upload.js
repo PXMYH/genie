e3cc0fdecc8ea1e1c4d6f803f5960b2915facf99
Michael Hu <pxmyhdev@gmail.com>
2019-12-17 15:19:30 +0800

    process uploaded files for project names and categories #1

--- routes/upload.js
+++ routes/upload.js
@@ -27,36 +27,71 @@ router.get('/files', function(req, res, next) {
   res.send(JSON.stringify(result, 2, 2));
 });
 
+function parseFile(result) {
+  var projectName = [];
+  console.debug('processing sheet content ...');
+  // get keys
+  sheetNames = Object.keys(result);
+  console.debug(`sheet names: ${sheetNames}`);
+
+  sheetNames.forEach(sheetName => {
+    // get categories
+    categories = result[sheetName][0];
+    console.debug(`categories: ${categories}`);
+
+    // get project name
+    for (var i = 1; i < result[sheetName].length; i++) {
+      // the first row will have the project name
+      if (result[sheetName][i][0] != undefined) {
+        console.debug(
+          `pushing ${result[sheetName][i][0]} to porject names ...`
+        );
+        projectName.push(result[sheetName][i][0].replace(/(^\s*)|(\s*$)/g, ''));
+      }
+    }
+    console.debug(`project names are ${projectName}`);
+
+    // get term end amount
+  });
+}
+
 router.post('/', (req, res, next) => {
   // parse a file uploaded
   var form = new formidable.IncomingForm();
   form.keepExtensions = true;
   form.encoding = 'utf-8';
 
-  form.parse(req, function(err, fields, files) {
+  form.parse(req, (err, fields, files) => {
+    // uncomment the below code block to inspect object passed in
     // res.end(
     //   util.inspect({
     //     fields: fields,
     //     files: files
     //   })
     // );
+
+    // require xlsx module
     if (typeof require !== 'undefined') {
       XLSX = require('xlsx');
-      console.log('initiating xlsx');
+      console.log('initiating xlsx ... ');
     }
-    console.log(`file path is ${JSON.stringify(files)}`);
+    console.debug(`file path is ${JSON.stringify(files)}`);
+
+    // parse the file uploaded
     var workbook = XLSX.readFile(files.upload.path);
-    console.log(`read workbook finished. workbook: ${workbook}`);
     var result = {};
+    console.debug(`read workbook finished. workbook: ${workbook}`);
+
     workbook.SheetNames.forEach(function(sheetName) {
-      console.log(`sheetname = ${sheetName}`);
-      var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {
+      console.debug(`Sheetname = ${sheetName}`);
+      var content = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {
         header: 1
       });
-      console.log(`roa: ${roa}`);
-      if (roa.length) result[sheetName] = roa;
+      console.debug(`Sheet content:\n ${content}`);
+      if (content.length) result[sheetName] = content;
     });
     res.send(JSON.stringify(result, 2, 2));
+    parseFile(result);
   });
 });
 
